38c307f988e86de36783282e9233d7b8ac1295fd
jasaf76 <gottfugthinzu@gmail.com>
2022-11-25 08:39:19 +0100

    final_connection_with_atlas

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const dotenv = require('dotenv');
 const app = require('./app');
+const mongoose = require('mongoose');
 // console.log(app.get("env"))
 
+dotenv.config({ path: "./config.env" });
 
-dotenv.config({ path: "./config.env" })
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+)
+mongoose
+  .connect(DB, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    // console.log(con.connection);
+    console.log("DB Connection Successfully");
+  });
+
+
 //console.log(process.env)
 const port = process.env.PORT || 3003;
 app.listen(port, () => {
