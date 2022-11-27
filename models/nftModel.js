1a03ccf3609fe9da9b79d116bb5c0a333cc09b88
jasaf76 <gottfugthinzu@gmail.com>
2022-11-27 02:15:11 +0100

    fertig

--- models/nftModel.js
+++ models/nftModel.js
@@ -5,8 +5,26 @@ const nftSchema = new mongoose.Schema({
     type: String,
     required: [true, "A NFT muss eine Haben"],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: String,
+    required: [true, "Aktuelle Dauer muss eine Haben"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "Aktuelle Größe muss eine Gruppe Haben"],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, "Aktuelle Dauer muss eine Gruppe Haben"],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 0,
+  },
+  ratingsQuantity: {
     type: Number,
     default: 0,
   },
@@ -14,6 +32,27 @@ const nftSchema = new mongoose.Schema({
     type: Number,
     required: [true, "A NFT muss ein Preis haben"],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required:[true, "A NFT muss provide the summary"],
+    trim: true,
+  },
+  Description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    trim: true,
+    required:[true, "A NFT muss provide the image cover"]
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates:[Date],
 });
 
 const NFT = mongoose.model("NFT", nftSchema);
