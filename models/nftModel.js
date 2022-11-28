422f07a69773d954f214fdc534a7787ed1c105d5
jasaf76 <gottfugthinzu@gmail.com>
2022-11-28 12:42:07 +0100

    complete

--- models/nftModel.js
+++ models/nftModel.js
@@ -1,59 +1,69 @@
 const mongoose = require("mongoose");
 
-const nftSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A NFT muss eine Haben"],
-    unique: true,
-    trim: true,
-  },
-  duration: {
-    type: String,
-    required: [true, "Aktuelle Dauer muss eine Haben"],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, "Aktuelle Größe muss eine Gruppe Haben"],
-  },
+const nftSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A NFT muss eine Haben"],
+      unique: true,
+      trim: true,
+    },
+    duration: {
+      type: String,
+      required: [true, "Aktuelle Dauer muss eine Haben"],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "Aktuelle Größe muss eine Gruppe Haben"],
+    },
 
-  difficulty: {
-    type: String,
-    required: [true, "Aktuelle Dauer muss eine Gruppe Haben"],
+    difficulty: {
+      type: String,
+      required: [true, "Aktuelle Dauer muss eine Gruppe Haben"],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 0,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, "A NFT muss ein Preis haben"],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      required: [true, "A NFT muss provide the summary"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      trim: true,
+      required: [true, "A NFT muss provide the image cover"],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
   },
-  ratingsAverage: {
-    type: Number,
-    default: 0,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, "A NFT muss ein Preis haben"],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    required: [true, "A NFT muss provide the summary"],
-    trim: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    trim: true,
-    required: [true, "A NFT muss provide the image cover"],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virutals: true },
+  }
+);
+
+nftSchema.virtual("durationWeeks").get(function () {
+  return this.duration / 7;
 });
 
 const NFT = mongoose.model("NFT", nftSchema);
