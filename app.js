2447f7179165d441198c5a976fe7cd41be8257b8
jasaf76 <gottfugthinzu@gmail.com>
2023-01-07 02:11:20 +0100

    helmet

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
-
+const rateLimit = require("express-rate-limit");
 const morgan = require("morgan");
 const AppError = require("./utils/AppError");
 const globalErrorHandler = require("./controllers/errorController");
-
+const helmet = require("helmet");
 const nftsRouter = require("./routes/nftsRoute");
 
 const usersRouter = require("./routes/usersRoute");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "10kb"}));
+//SET SECURITY HTTP HEADERS
+app.use(helmet());
 app.use(
   fileUpload({
     useTempFiles: true,
     tempFileDir: "./tmp",
   })
 );
+
+//GLOBAL MIDDLEWARES
+//LIMIT REQUEST FROM SAME API
 // if (process.env.NODE_ENV === 'development') {
 //   app.use(morgan("dev"));
 // }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message:
+    "Zu viele Anfragen von dieser IP, bitte versuchen Sie es in einer Stunde erneut!",
+});
+
+app.use("/api", limiter);
 
 app.use(morgan("dev"));
 //SERVING TEMPLATE DEMO
@@ -28,16 +42,16 @@ app.use(express.static(`${__dirname}/nft-data/img`));
 //Custom Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-     "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
+  // res.header("Access-Control-Allow-Origin", "*");
   // res.header(
-  //   "Access-Control-Allow-Methods",
-  //   "GET, POST, PUT,OPTIONS,DELETE,UPDATE,PATCH"
+  //    "Access-Control-Allow-Headers",
+  // //   "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
   // );
-  res.header("Allow", "GET, POST, PUT,OPTIONS,DELETE,UPDATE,PATCH");
+  // // res.header(
+  // //   "Access-Control-Allow-Methods",
+  // //   "GET, POST, PUT,OPTIONS,DELETE,UPDATE,PATCH"
+  // // );
+  // res.header("Allow", "GET, POST, PUT,OPTIONS,DELETE,UPDATE,PATCH");
 
   console.log("salio bien la cosa 🐶");
   //console.log(req.headers);
