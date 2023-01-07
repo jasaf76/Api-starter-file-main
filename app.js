c057b45ba19d022a79136921d9fb5c8083fd37db
jasaf76 <gottfugthinzu@gmail.com>
2023-01-07 09:46:57 +0100

    sanitize

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const rateLimit = require("express-rate-limit");
 const morgan = require("morgan");
-const AppError = require("./utils/AppError");
-const globalErrorHandler = require("./controllers/errorController");
 const helmet = require("helmet");
-const nftsRouter = require("./routes/nftsRoute");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
+const AppError = require("./utils/AppError");
+const nftsRouter = require("./routes/nftsRoute");
 const usersRouter = require("./routes/usersRoute");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
-app.use(express.json({ limit: "10kb"}));
+app.use(express.json({ limit: "10kb" }));
+
+//DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+
+//DATA SANITIZATION AGAINST XSS
+app.use(xss());
+
+
+
 //SET SECURITY HTTP HEADERS
 app.use(helmet());
+//File Upload
 app.use(
   fileUpload({
     useTempFiles: true,
